1633d80f3ba291e5808526ec8ae55111dc758738
sumin <68219918+MinKimS@users.noreply.github.com>
2022-12-07 06:06:55 +0900

    총 발사 딜레이

--- Assets/Scripts/FireCtrl.cs
+++ Assets/Scripts/FireCtrl.cs
@@ -11,6 +11,9 @@ public class FireCtrl : MonoBehaviour
 
     ParticleSystem fireEffect;
     AudioSource _audio;
+
+    // ÃÑ½î´Â µô·¹ÀÌ
+    bool isFire = true;
     void Start()
     {
         fireEffect = firePos.GetComponentInChildren<ParticleSystem>();
@@ -21,15 +24,24 @@ public class FireCtrl : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Fire();
+            if(isFire)
+            {
+                StartCoroutine(FireCheck());
+            }
         }
     }
     void Fire()
     {
-
         Instantiate(bullet, firePos.position, firePos.rotation);
         cartridge.Play();
         fireEffect.Play();
         _audio.PlayOneShot(fireSound, 1.0f);
+        isFire = false;
+    }
+
+    IEnumerator FireCheck()
+    {
+        yield return new WaitForSeconds(1.0f);
+        isFire = true;
     }
 }
